8a7ffddb63bc34870772e84f63a21ae66afb27b0
duytin <duytin@localhost>
2015-12-03 16:42:41 +0100

    extract_markers.py can print marker sequences

--- utils/extract_markers.py
+++ utils/extract_markers.py
@@ -4,36 +4,46 @@
 
 __author__  = 'Duy Tin Truong (duytin.truong@unitn.it)'
 __version__ = '0.1'
-__date__    = '27th Nov 2014'
+__date__    = '1 Sep 2014'
 
 import sys
 import os
 import argparse as ap
 import pickle
 import bz2
-
+from Bio import SeqIO, Seq, SeqRecord
 
 def read_params():
     p = ap.ArgumentParser()
-    p.add_argument('--clades', nargs='+', required=True, type=str)
-    p.add_argument('--mpa_pkl', required=True, type=str)
-    p.add_argument('--ofn_markers', required=True, type=str)
-
+    p.add_argument('--mpa_pkl', required=True, default=None, type=str)
+    p.add_argument('--ifn_markers', required=False, default=None, type=str)
+    p.add_argument('--clade', required=True, default=None, type=str)
+    p.add_argument('--ofn_markers', required=True, default=None, type=str)
     return vars(p.parse_args())
 
 
-def main(args):
-    with open(args['mpa_pkl'], 'rb') as ifile:
+def extract_markers(mpa_pkl, ifn_markers, clade, ofn_markers):
+    with open(mpa_pkl, 'rb') as ifile:
         db = pickle.loads(bz2.decompress(ifile.read()))
-    with open(args['ofn_markers'], 'w') as ofile:
-        for marker in db['markers']:
-            for clade in args['clades']:
-                if clade in db['markers'][marker]['taxon']:
-                    ofile.write('%s\n'%marker)
-                    break
-
+    markers = set([])
+    for marker in db['markers']:
+        if clade == db['markers'][marker]['taxon'].split('|')[-1]:
+            markers.add(marker)
+    print 'number of markers', len(markers)
+    with open(ofn_markers, 'w') as ofile:
+        if ifn_markers:
+            for rec in SeqIO.parse(open(ifn_markers, 'r'), 'fasta'):
+                if rec.name in markers:
+                    SeqIO.write(rec, ofile, 'fasta')
+        else:
+            for m in markers:
+                ofile.write('%s\n'%m)
 
 
 if __name__ == "__main__":
     args = read_params()
-    main(args)
+    extract_markers(
+                    mpa_pkl=args['mpa_pkl'],
+                    ifn_markers=args['ifn_markers'],
+                    clade=args['clade'],
+                    ofn_markers=args['ofn_markers'])
