2f639bff2943ced0dd1b8650131947cc68618098
Francesco Asnicar <francesco.asnicar@gmail.com>
2017-11-08 11:37:40 +0100

    metaphlan2.py command line updated

--- strainphlan_tutorial/step2_fastq2sam.sh
+++ strainphlan_tutorial/step2_fastq2sam.sh
@@ -4,5 +4,5 @@ for f in $(ls fastqs/*.bz2)
 do
     echo "Running metaphlan2 on ${f}"
     bn=$(basename ${f} | cut -d . -f 1)
-    tar xjfO ${f} | ../metaphlan2.py --bowtie2db ../db_v20/mpa_v20_m200 --mpa_pkl ../db_v20/mpa_v20_m200.pkl --input_type multifastq --nproc 10 -s sams/${bn}.sam.bz2 --bowtie2out sams/${bn}.bowtie2_out.bz2 -o sams/${bn}.profile
+    tar xjfO ${f} | ../metaphlan2.py --input_type multifastq --nproc 10 -s sams/${bn}.sam.bz2 --bowtie2out sams/${bn}.bowtie2_out.bz2 -o sams/${bn}.profile
 done
