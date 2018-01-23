22eae8820cba6c0be10db9bc3084998566fe5b29
Francesco Asnicar <francesco.asnicar@gmail.com>
2018-01-23 17:49:34 +0100

    update version 2.7.2

--- metaphlan2.py
+++ metaphlan2.py
@@ -16,8 +16,8 @@ from __future__ import with_statement
 __author__ = ('Nicola Segata (nicola.segata@unitn.it), '
               'Duy Tin Truong, '
               'Francesco Asnicar (f.asnicar@unitn.it)')
-__version__ = '2.7.1'
-__date__ = '07 November 2017'
+__version__ = '2.7.2'
+__date__ = '23 January 2018'
 
 
 import sys
