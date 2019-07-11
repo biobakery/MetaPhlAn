5701c547473ef90ea2a37f3eb02ef2e7a6da6d79
aitor blancomiguez <9294156+abmiguez@users.noreply.github.com>
2019-07-11 14:26:18 +0200

    Adds StrainPhlAn compatibility with Python 2 and 3

--- strainphlan_src/fastx_len_filter.py
+++ strainphlan_src/fastx_len_filter.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
+#Author: Duy Tin Truong (duytin.truong@unitn.it)
+#        at CIBIO, University of Trento, Italy
+
+__author__ = ('Duy Tin Truong (duytin.truong@unitn.it), '
+              'Aitor Blanco Miguez (aitor.blancomiguez@unitn.it)')
+__version__ = '0.2'
+__date__    = '10 Jul 19'
+
+
 from Bio import SeqIO
 import argparse as ap
 import sys
@@ -12,6 +21,8 @@ if __name__ == '__main__':
 	args = read_params(sys.argv)
 	min_len = args['min_len']
 	with sys.stdout as outf:
+		list_r = []
 		for r in SeqIO.parse(sys.stdin, "fastq"):
 			if len(r) >= min_len:
-				SeqIO.write(r, outf, "fastq")
+				list_r.append(r)
+		SeqIO.write(list_r, outf, "fastq")
--- strainphlan_src/which.py
+++ strainphlan_src/which.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python2
-# Author: Duy Tin Truong (duytin.truong@unitn.it)
-#		at CIBIO, University of Trento, Italy
+#!/usr/bin/env python
+#Author: Duy Tin Truong (duytin.truong@unitn.it)
+#        at CIBIO, University of Trento, Italy
+
+__author__ = ('Duy Tin Truong (duytin.truong@unitn.it), '
+              'Aitor Blanco Miguez (aitor.blancomiguez@unitn.it)')
+__version__ = '0.2'
+__date__    = '10 Jul 19'
 
 
 import os
