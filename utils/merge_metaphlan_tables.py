15b6503797c33fb8ceec6c5ceff579fba73a9cc5
Francesco Beghini <fbeghini@outlook.com>
2019-07-29 15:21:19 +0200

    Fixed unknown estimation. New merge MetaPhlAn2 tables with Pandas

--- utils/merge_metaphlan_tables.py
+++ utils/merge_metaphlan_tables.py
@@ -1,104 +1,58 @@
 #!/usr/bin/env python
 
-# ==============================================================================
-# Merge script: from MetaPhlAn output on single sample to a joined "clades vs samples" table
-# Authors: Timothy Tickle (ttickle@hsph.harvard.edu) and Curtis Huttenhower (chuttenh@hsph.harvard.edu)
-# ==============================================================================
-
 import argparse
-import csv
 import os
 import sys
 import re
+import pandas as pd
+from itertools import takewhile
 
-def merge( aaastrIn, astrLabels, iCol, ostm ):
+def merge( aaastrIn, ostm ):
     """
     Outputs the table join of the given pre-split string collection.
 
     :param  aaastrIn:   One or more split lines from which data are read.
     :type   aaastrIn:   collection of collections of string collections
-    :param  astrLabels: File names of input data.
-    :type   astrLabels: collection of strings
     :param  iCol:       Data column in which IDs are matched (zero-indexed).
     :type   iCol:       int
     :param  ostm:       Output stream to which matched rows are written.
     :type   ostm:       output stream
     """
 
-    setstrIDs = set()
-    """The final set of all IDs in any table."""
-    ahashIDs = [{} for i in range( len( aaastrIn ) )]
-    """One hash of IDs to row numbers for each input datum."""
-    aaastrData = [[] for i in range( len( aaastrIn ) )]
-    """One data table for each input datum."""
-    aastrHeaders = [[] for i in range( len( aaastrIn ) )]
-    """The list of non-ID headers for each input datum."""
-    strHeader = "ID"
-    """The ID column header."""
-
     listmpaVersion = set()
-    # For each input datum in each input stream...
-    pos = 0
-    dCol = None
+    merged_tables = pd.DataFrame()
 
     for f in aaastrIn:
-        with open(f) as csvfile:
-            iIn = csv.reader(csvfile, csv.excel_tab)
+        with open(f) as fin:
+            headers = [x.strip() for x in takewhile(lambda x: x.startswith('#'), fin)]
+     
+        mpaVersion = list(filter(re.compile('#mpa_v[0-9]{2,}_CHOCOPhlAn_[0-9]{0,}').match, headers))
+        if len(mpaVersion):
+            listmpaVersion.add(mpaVersion[0])
 
-            # Lines from the current file, empty list to hold data, empty hash to hold ids
-            aastrData, hashIDs = (a[pos] for a in (aaastrData, ahashIDs))
+        if len(listmpaVersion) > 1:
+            print('merge_metaphlan_tables found tables made with different versions of the MetaPhlAn2 database.\nPlease re-run MetaPhlAn2 with the same database.\n')
+            return
 
-            iLine = -1
-            # For a line in the file
-            for astrLine in iIn:
-                if astrLine[0].startswith('#'):
-                    dCol = astrLine.index('relative_abundance') if 'relative_abundance' in astrLine else None
-                    mpaVersion = astrLine[0] if re.match('#v[0-9]{2,}_CHOCOPhlAn_',astrLine[0]) else None
+        iIn = pd.read_csv(f, 
+                          sep='\t',
+                          skiprows=len(headers),
+                          names = headers[-1].split('#')[1].strip().split('\t'),
+                          index_col=0
+                        )
 
-                    if mpaVersion is not None:
-                        listmpaVersion.add(mpaVersion)
-                    if len(listmpaVersion) > 1:
-                        print('merge_metaphlan_tables found tables made with different versions of the MetaPhlAn2 database.\nPlease re-run MetaPhlAn2 with the same database.\n')
-                        return
-                    continue
+        merged_tables = pd.merge(merged_tables,
+                                 iIn.iloc[:,0].rename(os.path.splitext(os.path.basename(f))[0]), 
+                                 how='outer', 
+                                 left_index=True, 
+                                 right_index=True
+                                )
+        merged_tables.index.names = ['ID']
 
-                iLine += 1
+    if listmpaVersion:
+        ostm.write(list(listmpaVersion)[0])
 
-                if dCol:
-                    strID, astrData = astrLine[iCol], [astrLine[dCol]]
-                else:
-                    # ID is from first column, data are everything else
-                    strID, astrData = astrLine[iCol], (astrLine[:iCol] + astrLine[iCol + 1:])
-
-                hashIDs[strID] = iLine
-                aastrData.append(astrData)
-
-            # Batch merge every new ID key set
-            setstrIDs.update(hashIDs.keys())
-
-        pos += 1
-
-    # Create writer
-    csvw = csv.writer( ostm, csv.excel_tab, lineterminator='\n' )
-    # Write MetaPhlAn2+CHOCOPhlAn DB version
-    csvw.writerow(list(listmpaVersion))
-    # Make the file names the column names
-    csvw.writerow( [strHeader] + [os.path.splitext(f)[0] for f in astrLabels] )
-
-    # Write out data
-    for strID in sorted( setstrIDs ):
-        astrOut = []
-        for iIn in range( len( aaastrIn ) ):
-            aastrData, hashIDs = (a[iIn] for a in (aaastrData, ahashIDs))
-            # Look up the row number of the current ID in the current dataset, if any
-            iID = hashIDs.get( strID )
-            # If not, start with no data; if yes, pull out stored data row
-            astrData = [0.0] if ( iID == None ) else aastrData[iID]
-            # Pad output data as needed
-            astrData += [None] * ( len( aastrHeaders[iIn] ) - len( astrData ) )
-            astrOut += astrData
-        csvw.writerow( [strID] + astrOut )
-
+    ostm.write(merged_tables.fillna('0').reset_index().to_string(index=False))
 
 argp = argparse.ArgumentParser( prog = "merge_metaphlan_tables.py",
     description = """Performs a table join on one or more metaphlan output files.""")
@@ -114,7 +68,7 @@ argp.usage = argp.format_usage()[7:]+"\n\n\tPlease make sure to supply file path
 
 def _main( ):
     args = argp.parse_args( )
-    merge(args.aistms, [os.path.split(os.path.basename(f))[1] for f in args.aistms], 0, sys.stdout)
+    merge(args.aistms, sys.stdout)
 
 
 if __name__ == "__main__":
