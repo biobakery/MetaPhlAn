8b041c7cef3d74bf19b7f8fdee4f2720e47836a3
Francesco Beghini <fbeghini@outlook.com>
2020-03-18 18:56:39 +0100

    updates

--- setup.py
+++ setup.py
@@ -1,15 +1,143 @@
 import setuptools
+from setuptools.command.install import install
 from io import open
-import sys
+import sys, shutil, os, zipfile, tarfile, subprocess, tempfile, re, time
+from urllib.request import urlretrieve
 
-install_requires = ['biopython', 'pandas', 'scipy', 'requests', 'dendropy', 'pysam', 'biom-format']
+install_requires = ['numpy', 'h5py', 'biom-format', 'biopython', 'pandas', 'scipy', 'requests', 'dendropy', 'pysam']
 setup_requires = ['numpy', 'cython']
 
 if sys.version_info[0] < 3:
     sys.stdout.write('MetaPhlAn requires Python 3 or higher. Please update you Python installation')
 
+def byte_to_megabyte(byte):
+    """
+    Convert byte value to megabyte
+    """
+    
+    return byte / (1024.0**2)
 
- 
+class ReportHook():
+    def __init__(self):
+        self.start_time=time.time()
+        
+    def report(self, blocknum, block_size, total_size):
+        """
+        Print download progress message
+        """
+        
+        if blocknum == 0:
+            self.start_time=time.time()
+            if total_size > 0:
+                print("Downloading file of size: " + "{:.2f}".format(byte_to_megabyte(total_size)) + " MB\n")
+        else:
+            total_downloaded=blocknum*block_size
+            status = "{:3.2f} MB ".format(byte_to_megabyte(total_downloaded))
+                    
+            if total_size > 0:
+                percent_downloaded=total_downloaded * 100.0 / total_size
+                # use carriage return plus sys.stdout to overwrite stdout
+                try:
+                    download_rate=total_downloaded/(time.time()-self.start_time)
+                    estimated_time=(total_size-total_downloaded)/download_rate
+                except ZeroDivisionError:
+                    download_rate=0
+                    estimated_time=0
+                estimated_minutes=int(estimated_time/60.0)
+                estimated_seconds=estimated_time-estimated_minutes*60.0
+                status +="{:3.2f}".format(percent_downloaded) + " %  " + \
+                    "{:5.2f}".format(byte_to_megabyte(download_rate)) + " MB/sec " + \
+                    "{:2.0f}".format(estimated_minutes) + " min " + \
+                    "{:2.0f}".format(estimated_seconds) + " sec "
+            status+="        \r"
+            sys.stdout.write(status)
+
+def download(url, download_file):
+    """
+    Download a file from a url
+    """
+
+    try:
+        print("Downloading "+url)
+        file, headers = urlretrieve(url,download_file,reporthook=ReportHook().report)
+        # print final return to start new line of stdout
+        print("\n")
+    except EnvironmentError:
+        print("WARNING: Unable to download "+url)
+    
+def download_unpack_zip(url,download_file_name,folder,software_name):
+    """
+    Download the url to the file and decompress into the folder
+    """
+    
+    # Check for write permission to the target folder
+    if not os.access(folder, os.W_OK):
+        print("WARNING: The directory is not writeable: "+
+            folder + " . Please modify the permissions.")
+    
+    download_file=os.path.join(folder, download_file_name)
+    
+    download(url, download_file)
+    
+    error_during_extract=False
+    
+    try:
+        zipfile_handle=zipfile.ZipFile(download_file)
+        zipfile_handle.extractall(path=folder)
+        zipfile_handle.close()
+    except EnvironmentError:
+        print("WARNING: Unable to extract "+software_name+".")
+        error_during_extract=True
+        
+    if not error_during_extract:
+        try:
+            os.unlink(download_file)
+        except EnvironmentError:
+            print("WARNING: Unable to remove the temp download: " + download_file)
+       
+def download_unpack_tar(url,download_file_name,folder,software_name):
+    """
+    Download the url to the file and decompress into the folder
+    """
+    
+    # Check for write permission to the target folder
+    if not os.access(folder, os.W_OK):
+        print("WARNING: The directory is not writeable: "+
+            folder + " . Please modify the permissions.")
+    
+    download_file=os.path.join(folder, download_file_name)
+    
+    download(url, download_file)
+    
+    error_during_extract=False
+    
+    try:
+        tarfile_handle=tarfile.open(download_file)
+        tarfile_handle.extractall(path=folder)
+        tarfile_handle.close()
+    except (EnvironmentError,tarfile.ReadError):
+        print("WARNING: Unable to extract "+software_name+".")
+        error_during_extract=True
+        
+    if not error_during_extract:
+        try:
+            os.unlink(download_file)
+        except EnvironmentError:
+            print("WARNING: Unable to remove the temp download: " + download_file)
+
+class Install(install):
+    def run(self):  
+        print('Installing cmseq')
+        cmseq_repo = "https://github.com/SegataLab/cmseq/archive/master.zip"
+        cmseq_path = os.path.dirname(os.path.abspath(__file__))
+        print(os.path.abspath(__file__))
+        # cmseq_path = os.path.join(self.install_base, "lib", "python{}.{}".format(sys.version_info[0], sys.version_info[1]), "site-packages", "metaphlan","utils", 'cmseq')
+        if os.path.exists(cmseq_path):
+            os.rmdir(cmseq_path)
+        else:
+            download_unpack_zip(cmseq_repo, "cmseq.zip", cmseq_path, "cmseq")
+        install.run(self)
+
 setuptools.setup(
     name='MetaPhlAn',
     version='3.0',
@@ -21,6 +149,9 @@ setuptools.setup(
     package_data = { 'MetaPhlAn' : [
         'metaphlan/utils/*'
     ]},
+    cmdclass={
+        'biocondainstall': Install
+    },
     entry_points={
         'console_scripts': [
             'metaphlan = metaphlan.metaphlan:main',
