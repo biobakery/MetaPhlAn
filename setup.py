f8cfa653a8e2f290883b0b61c2fc9837fc809525
Aitor Blanco-Miguez <aitor.blancomiguez@unitn.it>
2024-03-11 14:44:26 +0000

    Updates recipes

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setuptools.setup(
             'run_treeshrink.py = metaphlan.utils.treeshrink.run_treeshrink:main',
             'treeshrink.py = metaphlan.utils.treeshrink.treeshrink:main',
             'create_toy_database.py = metaphlan.utils.create_toy_database:main',
+            'fix_relab_mpa4.py = metaphlan.utils.fix_relab_mpa4:main',
         ]
     },
     description='MetaPhlAn is a computational tool for profiling the composition of microbial communities (Bacteria, Archaea and Eukaryotes) from metagenomic shotgun sequencing data (i.e. not 16S) with species-level. With the newly added StrainPhlAn module, it is now possible to perform accurate strain-level microbial profiling.',
