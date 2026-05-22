82131d1891c27167f7c31cd8a832001b10581a5c
Cengoni <claudia.mengoni@unitn.it>
2026-05-22 12:02:38 +0200

    make sgb_to_gtdb more robust

--- metaphlan/utils/sgb_to_gtdb_profile.py
+++ metaphlan/utils/sgb_to_gtdb_profile.py
@@ -6,10 +6,10 @@ __date__    = '21 Oct 2025'
 import os
 import time
 import argparse as ap
-try:
+'''try:
     from .util_fun import info, error
 except ImportError:
-    from util_fun import info, error
+    from util_fun import info, error'''
 
 GTDB_ASSIGNMENT_FILE = os.path.join(os.path.dirname(os.path.abspath(
     __file__)), "mpa_vJan25_CHOCOPhlAnSGB_202503_SGB2GTDB.tsv")
@@ -50,6 +50,13 @@ def get_gtdb_profile(mpa_profile, gtdb_profile):
         gtdb_profile (str): the path to the output GTDB profile
     """
     tax_levels = ['d', 'p', 'c', 'o', 'f', 'g', 's']
+    def get_parent_taxon(taxon, level):
+        parts = taxon.split(';')
+        if len(parts) > 1:
+            return ';'.join(parts[:-1])
+        else:
+            return parts[0]
+
     sgb2gtdb = dict()
     with open(GTDB_ASSIGNMENT_FILE, 'r') as read_file:
         for line in read_file:
@@ -68,19 +75,22 @@ def get_gtdb_profile(mpa_profile, gtdb_profile):
                     wf.write('UNCLASSIFIED\t{}\n'.format(unclassified))
                 elif 't__SGB' in line:
                     line = line.strip().split('\t')
-                    gtdb_tax = sgb2gtdb[line[0].split('|')[-1][3:]]
-                    if gtdb_tax not in abundances:
+                    abundance = float(line[2])
+                    sgb_id = line[0].split('|')[-1][3:]
+                    gtdb_tax = sgb2gtdb.get(sgb_id, None)
+                    if gtdb_tax is None:
+                        continue
+                    if gtdb_tax not in abundances['s']:
                         abundances['s'][gtdb_tax] = 0
-                    abundances['s'][gtdb_tax] += float(line[2])
-        tax_levels.reverse()
-        for tax_level in tax_levels[:-1]:
+                    abundances['s'][gtdb_tax] += abundance
+        tax_levels_rev = list(reversed(tax_levels))
+        for i, tax_level in enumerate(tax_levels_rev[:-1]):
             for tax in abundances[tax_level]:
-                new_tax = tax.replace(';{}'.format(tax.split(';')[-1]), '')
-                new_level = tax_levels[tax_levels.index(tax_level)+1]
-                if new_tax not in abundances[new_level]:
-                    abundances[new_level][new_tax] = 0
-                abundances[new_level][new_tax] += abundances[tax_level][tax]
-        tax_levels.reverse()
+                parent_tax = get_parent_taxon(tax, tax_level)
+                new_level = tax_levels_rev[i+1]
+                if parent_tax not in abundances[new_level]:
+                    abundances[new_level][parent_tax] = 0
+                abundances[new_level][parent_tax] += abundances[tax_level][tax]
         for tax_level in tax_levels:
             for tax in abundances[tax_level]:
                 wf.write('{}\t{}\n'.format(tax, abundances[tax_level][tax]))
@@ -89,11 +99,11 @@ def get_gtdb_profile(mpa_profile, gtdb_profile):
 def main():
     t0 = time.time()
     args = read_params()
-    info("Start execution")
+    #info("Start execution")
     check_params(args)
     get_gtdb_profile(args.input, args.output)
     exec_time = time.time() - t0
-    info("Finish execution ({} seconds)".format(round(exec_time, 2)))
+    #info("Finish execution ({} seconds)".format(round(exec_time, 2)))
 
 
 if __name__ == "__main__":
